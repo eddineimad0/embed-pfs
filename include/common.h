850d8c49904927b9ff357236a8b38ee47ce70de9
eddineimad0 <61315998+eddineimad0@users.noreply.github.com>
2023-11-03 23:04:18 +0100

    first commit.

---
+++ include/common.h
@@ -0,0 +1,7 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include<stdint.h>
+#include<stdbool.h>
+
+#endif // !__COMMON_H__
---
+++ src/firmware.c
@@ -0,0 +1,6 @@
+/*
+ * Firmware Entry point
+ */
+int main(void){
+    return 0;
+}
